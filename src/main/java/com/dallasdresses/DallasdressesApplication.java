e57cfe79e6ee01482322e44a4e18c71862dc7992
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-07 15:16:29 -0500

    Initial commit.

---
+++ src/main/java/com/dallasdresses/DallasdressesApplication.java
@@ -0,0 +1,13 @@
+package com.dallasdresses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DallasdressesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DallasdressesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dallasdresses/DallasdressesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dallasdresses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DallasdressesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
