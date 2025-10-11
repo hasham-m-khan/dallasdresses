ca2dd4b8fad737cc4bf4eeb1ef72d1756b286eef
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-11 11:36:04 -0500

    Updates dummy data with role strings

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) VALUES (
     'john.doe@xyzmail.com', 'John', 'Doe',
-    1, 'en', true);
+    'USER', 'en', true);
 
 INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) VALUES (
       'danille.jackson@xyzmail.com', 'Danielle', 'Jackson',
-      0, 'en', true);
+      'STAFF', 'en', true);
