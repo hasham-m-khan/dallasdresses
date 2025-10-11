6f725b2af314f185d762437e6eb276221201c2ed
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-11 04:17:15 -0500

    Removes ids from row insertions.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) VALUES (
+    'john.doe@xyzmail.com', 'John', 'Doe',
+    1, 'en', true);
+
+INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) VALUES (
+      'danille.jackson@xyzmail.com', 'Danielle', 'Jackson',
+      0, 'en', true);
