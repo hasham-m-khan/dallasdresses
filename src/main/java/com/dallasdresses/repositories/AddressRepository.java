7b3b92d7f67e2629015d8389c2e4fbaed988911e
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-15 20:58:38 -0500

    Updates address repository for duplicate checks.

--- src/main/java/com/dallasdresses/repositories/AddressRepository.java
+++ src/main/java/com/dallasdresses/repositories/AddressRepository.java
@@ -2,6 +2,24 @@ package com.dallasdresses.repositories;
 
 import com.dallasdresses.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    public Optional<List<Address>> findByUserId(Long userId);
+
+    Optional<Address> findByUserIdAndAddressLine1AndCityAndStateAndPostalCode(
+            Long userId,
+            String addressLine1,
+            String city,
+            String state,
+            String postalCode
+    );
+
+    Optional<Address> findByUserIdAndAddressType(Long userId, String addressType);
+
+    boolean existsByUserId(Long userId);
 }
