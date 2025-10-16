8320c59f56fe8a67709ead5de1883ad62f6a5480
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-15 20:59:43 -0500

    Adds default repositories.

--- src/main/java/com/dallasdresses/repositories/ItemImageRepository.java
+++ src/main/java/com/dallasdresses/repositories/ItemImageRepository.java
@@ -2,6 +2,8 @@ package com.dallasdresses.repositories;
 
 import com.dallasdresses.entities.ItemImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
 }
