5b4a9a1d6ae6154d77eab773f6658dda109d066d
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-15 20:55:19 -0500

    Adds address dummy data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,19 @@ INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) V
 INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) VALUES (
       'danille.jackson@xyzmail.com', 'Danielle', 'Jackson',
       'STAFF', 'en', true);
+
+INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) VALUES (
+      'Jeremiah.Daniels@xyzmail.com', 'Jeremiah', 'Daniels',
+      'USER', 'en', true);
+
+INSERT INTO addresses (ADDRESS_LINE1, CITY, STATE, COUNTRY, POSTAL_CODE, ADDRESS_TYPE, USER_ID)
+    VALUES ('123 Main Street', 'Queens', 'NY', 'USA', '11234',
+            'RESIDENTIAL', 1);
+
+INSERT INTO addresses (ADDRESS_LINE1, ADDRESS_LINE2, CITY, STATE, COUNTRY, POSTAL_CODE, ADDRESS_TYPE, USER_ID)
+VALUES ('W 42nd Street', 'Building #434', 'New York', 'NY', 'USA',
+        '10036', 'ALTERNATE', 1);
+
+INSERT INTO addresses (ADDRESS_LINE1, CITY, STATE, COUNTRY, POSTAL_CODE, ADDRESS_TYPE, USER_ID)
+VALUES ('1060 W Addison ST', 'Chicago', 'IL', 'USA', '60613',
+        'RESIDENTIAL', 2);
