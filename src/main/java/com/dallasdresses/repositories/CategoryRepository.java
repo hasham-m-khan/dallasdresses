fa26e8087f960933f90e84891f3548487a887716
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-20 19:20:05 -0500

    Updates categories repositories with new get methods.

--- src/main/java/com/dallasdresses/repositories/CategoryRepository.java
+++ src/main/java/com/dallasdresses/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.dallasdresses.repositories;
 import com.dallasdresses.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+    Optional<Category> findBySlug(String slug);
 }
