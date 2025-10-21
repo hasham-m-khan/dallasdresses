041086cec7c11d1f08d10eb298956501b4b9b750
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-20 19:19:31 -0500

    Adds builder pattern to categories.

--- src/main/java/com/dallasdresses/entities/Category.java
+++ src/main/java/com/dallasdresses/entities/Category.java
@@ -2,13 +2,19 @@ package com.dallasdresses.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
