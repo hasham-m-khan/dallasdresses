cea8084c785c45fc9f91bb713de12d876b8aee16
Hasham Khan <hasham.m.khan@outlook.com>
2025-10-21 15:19:06 -0500

    Adds category dummy data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- Users Table
 INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) VALUES (
     'john.doe@xyzmail.com', 'John', 'Doe',
     'USER', 'en', true);
@@ -10,6 +11,8 @@ INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, ROLE, LOCALE, EMAIL_VERIFIED) V
       'Jeremiah.Daniels@xyzmail.com', 'Jeremiah', 'Daniels',
       'USER', 'en', true);
 
+
+-- Addresses Table
 INSERT INTO addresses (ADDRESS_LINE1, CITY, STATE, COUNTRY, POSTAL_CODE, ADDRESS_TYPE, USER_ID)
     VALUES ('123 Main Street', 'Queens', 'NY', 'USA', '11234',
             'RESIDENTIAL', 1);
@@ -21,3 +24,10 @@ VALUES ('W 42nd Street', 'Building #434', 'New York', 'NY', 'USA',
 INSERT INTO addresses (ADDRESS_LINE1, CITY, STATE, COUNTRY, POSTAL_CODE, ADDRESS_TYPE, USER_ID)
 VALUES ('1060 W Addison ST', 'Chicago', 'IL', 'USA', '60613',
         'RESIDENTIAL', 2);
+
+
+-- Categories Table
+INSERT INTO categories (NAME, SLUG) VALUES ('Jackets', 'jackets');
+INSERT INTO categories (NAME, SLUG) VALUES ('Jeans', 'jeans');
+INSERT INTO categories (NAME, SLUG) VALUES ('Mens', 'mens');
+INSERT INTO categories (NAME, SLUG) VALUES ('Womens', 'womens');
