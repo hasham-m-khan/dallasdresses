5450428a3ac1f02464a29e8912e89e0c199f3036
Hasham Khan <hasham.m.khan@outlook.com>
2025-12-24 13:56:23 -0600

    Adds item variants to entity and updates not null checks.

--- src/main/java/com/dallasdresses/entities/Item.java
+++ src/main/java/com/dallasdresses/entities/Item.java
@@ -1,8 +1,10 @@
 package com.dallasdresses.entities;
 
-import com.dallasdresses.entities.enums.enums.DiscountType;
+import com.dallasdresses.entities.enums.DiscountType;
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -11,32 +13,49 @@ import java.sql.Timestamp;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "items")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
 
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private Item parentItem;
-
+    @NotBlank
     private String name;
 
     @Lob
+    @NotBlank
     private String description;
 
     @Lob
+    @NotBlank
     private String summary;
 
+    @NotNull
     private BigDecimal price;
 
+    @NotNull
+    @Enumerated(EnumType.STRING)
     private DiscountType discountType;
 
+    @NotNull
     private Double discountAmount;
 
-    @ManyToMany
+    @Builder.Default
+    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<ItemVariant> variants = new HashSet<>();
+
+    @NotNull
+    @Builder.Default
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
             name = "item_category",
             joinColumns = @JoinColumn(name = "item_id"),
@@ -44,7 +63,9 @@ public class Item {
     )
     private Set<Category> categories = new HashSet<>();
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+    @NotNull
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item", orphanRemoval = true, fetch = FetchType.LAZY)
+    @Builder.Default
     private Set<ItemImage> itemImages = new HashSet<>();
 
     @CreationTimestamp
@@ -52,4 +73,19 @@ public class Item {
 
     @UpdateTimestamp
     private Timestamp updatedAt;
+
+    public void addVariant(ItemVariant variant) {
+        this.variants.add(variant);
+        variant.setItem(this);
+    }
+
+    public void addImage(ItemImage itemImage) {
+        itemImages.add(itemImage);
+        itemImage.setItem(this);
+    }
+
+    public void addCategory(Category category) {
+        categories.add(category);
+        category.getItems().add(this);
+    }
 }
