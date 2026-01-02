45c83c3f2293fc4435087d0b91c2e6b3c0f1454e
Hasham Khan <hasham.m.khan@outlook.com>
2026-01-02 03:07:30 -0600

    Adds null checks to dto converter.

--- src/main/java/com/dallasdresses/converters/ItemToItemDtoConverter.java
+++ src/main/java/com/dallasdresses/converters/ItemToItemDtoConverter.java
@@ -6,6 +6,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Component
@@ -26,15 +27,26 @@ public class ItemToItemDtoConverter implements Converter<Item, ItemDto> {
                 .id(source.getId())
                 .name(source.getName())
                 .description(source.getDescription())
+                .color(source.getColor())
+                .size(source.getSize())
+                .stock(source.getStock())
                 .price(source.getPrice())
                 .discountType(source.getDiscountType())
                 .discountValue(source.getDiscountValue())
-                .categories(source.getCategories().stream()
-                        .map(categoryDtoConverter::convert)
-                        .collect(Collectors.toSet()))
-                .itemImages(source.getItemImages().stream()
-                        .map(imageDtoConverter::convert)
-                        .collect(Collectors.toSet()))
+                .parentId(source.getParent() != null ? source.getParent().getId() : null)
+                .categories(source.getCategories() != null
+                        ? source.getCategories().stream()
+                            .map(categoryDtoConverter::convert)
+                            .collect(Collectors.toSet())
+                        : Collections.emptySet())
+
+                .itemImages(source.getItemImages() != null
+                        ? source.getItemImages().stream()
+                            .map(imageDtoConverter::convert)
+                            .collect(Collectors.toSet())
+                        : Collections.emptySet())
+                .createdAt(source.getCreatedAt())
+                .updatedAt(source.getUpdatedAt())
                 .build();
     }
 }
