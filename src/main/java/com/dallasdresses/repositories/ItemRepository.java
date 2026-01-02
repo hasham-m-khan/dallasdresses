e76dd8ae8f26685249806239956f55f4d789b9cc
Hasham Khan <hasham.m.khan@outlook.com>
2026-01-02 03:06:12 -0600

    Updates JPA query method to check for duplicates.

--- src/main/java/com/dallasdresses/repositories/ItemRepository.java
+++ src/main/java/com/dallasdresses/repositories/ItemRepository.java
@@ -2,6 +2,7 @@ package com.dallasdresses.repositories;
 
 import com.dallasdresses.entities.Item;
 import com.dallasdresses.entities.enums.DiscountType;
+import com.dallasdresses.entities.enums.DressSize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,9 +19,5 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("SELECT i FROM Item i JOIN i.categories c WHERE c.slug = :slug")
     List<Item> findItemsByCategorySlug(String slug);
 
-    boolean existsByNameAndPriceAndDiscountType(
-            String name,
-            BigDecimal price,
-            DiscountType discountType
-    );
+    boolean existsByNameAndColorAndSize(String name, String color, DressSize size);
 }
