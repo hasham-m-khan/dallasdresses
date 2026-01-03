530052a5e967e71458067f8a403a662c6b4db6eb
Hasham Khan <hasham.m.khan@outlook.com>
2026-01-03 02:30:31 -0600

    Adds findAllByParentId to repository.

--- src/main/java/com/dallasdresses/repositories/ItemRepository.java
+++ src/main/java/com/dallasdresses/repositories/ItemRepository.java
@@ -1,13 +1,11 @@
 package com.dallasdresses.repositories;
 
 import com.dallasdresses.entities.Item;
-import com.dallasdresses.entities.enums.DiscountType;
 import com.dallasdresses.entities.enums.DressSize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -18,6 +16,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query("SELECT i FROM Item i JOIN i.categories c WHERE c.slug = :slug")
     List<Item> findItemsByCategorySlug(String slug);
+
+    List<Item> findAllByParentId(Long id);
 
     boolean existsByNameAndColorAndSize(String name, String color, DressSize size);
 }
