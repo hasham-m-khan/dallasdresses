624abc39a06e9c34c13f5d9efef10583cf43af6f
Hasham Khan <hasham.m.khan@outlook.com>
2026-02-12 15:43:01 -0600

    Makes fields private

--- src/main/java/com/dallasdresses/dtos/response/CategoryDto.java
+++ src/main/java/com/dallasdresses/dtos/response/CategoryDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDto {
-    Long id;
-    String name;
-    String slug;
+    private Long id;
+    private String name;
+    private String slug;
 }
