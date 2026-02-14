a8636038adda28c0261b4fa158969dccacddee21
Hasham Khan <hasham.m.khan@outlook.com>
2026-02-14 06:07:39 -0600

    Adds JPA query methods to repository.

--- src/main/java/com/dallasdresses/repositories/CartItemRepository.java
+++ src/main/java/com/dallasdresses/repositories/CartItemRepository.java
@@ -2,6 +2,16 @@ package com.dallasdresses.repositories;
 
 import com.dallasdresses.entities.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByCartIdAndItemId(Long cartId, Long itemId);
+    boolean existsByCartIdAndItemId(Long cartId, Long itemId);
+    void deleteByCartIdAndItemId(Long cartId, Long itemId);
+    void deleteByItemId(Long itemId);
+    void deleteByCartId(Long cartId);
 }
